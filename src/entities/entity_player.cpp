38e3a9415d2827102da583d22018c04caee22d3d
TimoGuy <timoguy99004@gmail.com>
2025-03-17 19:08:40 -0700

    Some restruct and adds cglm.

--- src/entities/entity_player.cpp
+++ src/entities/entity_player.cpp
@@ -3,7 +3,7 @@
 
 Entity_player::Entity_player()
     : Simulating_entity_ifc{}
-    , m_world_entity{ false }
+    , World_entity{ false }
 {
 
 }
@@ -15,7 +15,7 @@ void Entity_player::on_sim_entity_start(pool_elem_key_t key)
 
 void Entity_player::tick_sim_entity()
 {
-
+    
 }
 
 void Entity_player::on_sim_entity_destroy()
--- src/entities/entity_player.h
+++ src/entities/entity_player.h
@@ -4,17 +4,19 @@
 #include "world_entity.h"
 
 
-class Entity_player : public Simulating_entity_ifc
+class Entity_player
+    : public Simulating_entity_ifc
+    , public World_entity
 {
 public:
     Entity_player();
 
-    void on_sim_entity_start(pool_elem_key_t key) override;
-    void tick_sim_entity() override;
-    void on_sim_entity_destroy() override;
-
-    World_entity m_world_entity;
+    virtual void on_sim_entity_start(pool_elem_key_t key) override;
+    virtual void tick_sim_entity() override;
+    virtual void on_sim_entity_destroy() override;
 
 private:
     pool_elem_key_t m_sim_entity_key;
+
+    
 };
