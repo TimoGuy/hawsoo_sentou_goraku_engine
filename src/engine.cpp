cc90d16810a029a1d74ab2425f75cdcb74d8aed2
TimoGuy <timoguy99004@gmail.com>
2025-03-28 00:46:49 -0700

    WIP: NOCHECKIN: Adds a simple entity from behaviors but I think that the creation process is a little stupid ngl (requiring unique ptrs for everything??? That's bad)

--- src/engine.cpp
+++ src/engine.cpp
@@ -13,6 +13,13 @@
 #include "ticking_world_simulation_public.h"
 #include "world_entity_public.h"
 
+// @NOCHECKIN: ASDFASDFASDF //
+#include "simulating_ifc.h"
+#include <memory>
+#include "standard_behaviors.h"
+#include "physics_objects.h"
+//////////////////////////////
+
 
 // Impl.
 class Engine::Impl
@@ -73,6 +80,47 @@ public:
             fallback_screen_height
         };
         World_simulation simulation{ num_threads };
+
+        // @TODO: @NOCHECKIN: Sample level. //
+        class Player_entity : public simulating::Entity_ifc
+        {
+        public:
+            std::vector<simulating::Behavior_ifc*> on_create(size_t creation_idx)
+            {
+                m_phys_actor =
+                    std::make_unique<phys_obj::Actor_character_controller>(
+                        JPH::RVec3{ 0.0f, 0.0f, 0.0f },
+                        phys_obj::ACTOR_CC_TYPE_PLAYER,
+                        phys_obj::Shape_params_cylinder{
+                            .radius = 0.5f,
+                            .half_height = 1.0f,
+                        });
+                m_input = std::make_unique<std_behavior::Gamepad_input_behavior>();
+                m_movement = std::make_unique<std_behavior::Humanoid_movement>(*m_phys_actor);
+                m_animator = std::make_unique<std_behavior::Humanoid_animator>();
+
+                m_input->set_output(m_movement->get_data_key());
+                m_movement->set_output(m_animator->get_data_key());
+
+                return { m_input.get(), m_movement.get(), m_animator.get(), };
+            }
+
+            std::vector<simulating::Behavior_ifc*> on_teardown()
+            {
+                // @TODO: Remove behaviors and entity.
+                assert(false);
+                return { m_input.get(), m_movement.get(), m_animator.get(), };
+            }
+
+        private:
+            std::unique_ptr<std_behavior::Gamepad_input_behavior> m_input{ nullptr };
+            std::unique_ptr<std_behavior::Humanoid_movement> m_movement{ nullptr };
+            std::unique_ptr<std_behavior::Humanoid_animator> m_animator{ nullptr };
+            std::unique_ptr<phys_obj::Actor_character_controller> m_phys_actor{ nullptr };
+        };
+        simulation.add_sim_entity_to_world(std::make_unique<Player_entity>());
+        //////////////////////////////////////
+
         job_sources.emplace_back(&renderer);
         job_sources.emplace_back(&simulation);
 
